4ed935c92b516c761b5c8478bdd10834fa95d1b4
champix56 <desorbaix@free.fr>
2023-12-04 14:40:54 +0100

    commit

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import './App.css';
-import Button from './components/ui/Button/Button';
+import React from "react";
+import "./App.css";
+import Button from "./components/ui/Button/Button";
 
-
-const App=(props)=>{
+const App = (props) => {
   return (
-    <div className="App">Demat Breizh
-      <Button text="Coucou"/>
-      <Button text="benjamin"/>
-      <Button text="Submit"/>
-      <Button text="reset"/>
+    <div className="App">
+      Demat Breizh
+      <hr />
+      <Button text="Submit" bgColor="skyblue" />
+      <Button text="reset" bgColor="tomato" />
     </div>
   );
-}
+};
 
 export default App;
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./Button.module.css";
+
 const Button = (props) => {
   console.log(props);
-  return <button className={styles.Button}>{props.text}</button>;
+/*
+  let bgColor = undefined;
+  switch (props.model) {
+    case "primary":
+      bgColor = "skyblue";
+      break;
+    case "secondary":
+      bgColor = "skyblue";
+      break;
+    default:
+      bgColor = "skyblue";
+      break;
+  }*/
+  return (
+    <button
+      className={styles.Button}
+      style={{ backgroundColor: props.bgColor }}
+    >
+      {props.text}
+    </button>
+  );
 };
 
 export default Button;
