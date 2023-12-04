7db5eb99c19027ac1dbe6b26218b156367274c0a
champix56 <desorbaix@free.fr>
2023-12-04 12:19:46 +0100

    restructuration

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App=(props)=>{
+  return (
+    <div className="App">Demat
+    <ul className='ul'>
+        <li className='li'>abc</li>
+
+    </ul> Breizh</div>
+  );
+}
+
+export default App;
