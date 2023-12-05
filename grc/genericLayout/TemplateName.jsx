92fefe4acfcb9e32d23916ea993d75530978bf50
champix56 <desorbaix@free.fr>
2023-12-05 10:41:09 +0100

    generations files

---
+++ grc/genericLayout/TemplateName.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from './TemplateName.module.css'
+/**
+ * TemplateName component
+ * @param {{children:object}} props
+ * @returns React.Element
+ */
+const TemplateName = (props) => {
+  return (
+    <div className={styles.TemplateName} data-testid="TemplateName" style={props.style}>
+      {props.children}
+    </div>
+  );
+};
+
+TemplateName.propTypes = {
+  children: PropTypes.any.isRequired,
+  style: PropTypes.object,
+};
+TemplateName.defaultProps = {};
+export default TemplateName;
