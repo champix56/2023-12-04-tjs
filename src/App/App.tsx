6630cea2bdf9d2559b1e79c48cc34c16ca942779
champix56 <desorbaix@free.fr>
2023-12-05 14:44:21 +0100

    passage ts avec maintient de state applicatif

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import FlexHGrow3 from "./components/layout/FlexHGrow3/FlexHGrow3";
 import Header from "./components/ui/Header/Header";
@@ -7,16 +7,20 @@ import FlexWGrow1 from "./components/layout/FlexWGrow1/FlexWGrow1";
 import Footer from "./components/ui/Footer/Footer";
 import MemeSvgViewer from "./components/ui/MemeSvgViewer/MemeSvgViewer";
 import MemeForm from "./components/feature/MemeForm/MemeForm";
+import { emptyMeme, MemeInterface } from "orsys-tjs-meme";
+interface IAppProps{}
 
-const App = () => {
+const App : React.FC<IAppProps>= (props) => {
+  const [memeState, setMemeState] = useState<MemeInterface>(emptyMeme)
+  console.log(memeState)
   return (
     <div className="App">
       <FlexHGrow3>
         <Header />
         <Navbar />
         <FlexWGrow1>
-          <MemeSvgViewer/>
-          <MemeForm/>
+          <MemeSvgViewer meme={memeState}/>
+          <MemeForm meme={memeState}/>
         </FlexWGrow1>
         <Footer/>
       </FlexHGrow3>
